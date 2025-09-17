fdc80d197dda9b74173d48188ad13404917b1ccf
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-17 08:05:51 +0200

    Show error page

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,6 +1,11 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 const port = 3333
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'error.html'))
+})
 
 app.listen(port, ()=>{
     console.log("Szerver mükszik ezen a porton:", port)
