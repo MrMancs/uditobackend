6a8d7340dacfa8534588bfecc9d0c8dfa68de476
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-18 09:06:41 +0200

    Fix something

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -101,6 +101,83 @@ app.post('/udito/:id', (req, res) => {
     
 })
 
+app.patch("/udito/:id", (req, res) => {
+    const data = fs.readFileSync("uditok.txt")
+    const responseBodyArr = this.uditoDataRead(data)
+    console.log("responseBodyArr", responseBodyArr)
+    console.log("params", req.params)
+    const id = +req.params.id
+    console.log("id", id)
+
+    const foundIndex = responseBodyArr.findIndex(udito => +udito.id == +id)
+    console.log("foundIndex", foundIndex)
+    if (foundIndex < 0) res.status(400).json({error: "id not found"})
+    else {
+        console.log("req.body", req.body)
+        const responseBody = {id, ...req.body}
+        console.log("responsebody", responseBody)
+
+        responseBodyArr.splice(foundIndex, 1, responseBody)
+        console.log("responsebodyarr after splice", responseBodyArr)
+
+        /*
+        const newFileLine = `${id};${req.body.nev};${req.body.liter};${req.body["bubis-e"]}`
+        try{
+            fs.appendFileSync("uditok.txt", newFileLine + "\n")
+        } catch(e) {
+            res.status(500).json({fileError: e})
+        }
+        */
+
+        let fileLines = ""
+        responseBodyArr.forEach(udito => {
+            const newFileLine = `${udito.id};${udito.nev};${udito.liter};${udito["bubis-e"]}\n`
+            fileLines += newFileLine
+        });
+
+        fs.writeFileSync("uditok.txt", fileLines)
+    }
+
+    res.sendStatus(200)
+})
+
+app.delete("/udito/:id", (req, res) => {
+    fs.readFile("uditok.txt", (err, data) => {
+        if (err) res.status(404).json({fileError: err})
+        else {
+            if (data) {
+                const responseBodyArr = uditoDataRead(data)
+                
+                console.log("params", req.params)
+                const id = +req.params?.id
+
+                const foundIndex = responseBodyArr.findIndex(udito => +udito.id == +id)
+
+                if (responseBodyArr.contains(found)) res.status(300).json({error: "existing id conflict"})
+
+                //res.status(200).json({error: "existing id conflict"})
+
+            } //else res.status(404).json({fileError: data}) 
+        }
+    })
+
+    //if id doesn't exist yet:
+    const id = +req.params?.id
+
+    const delFileLine = `${id};${req.body.nev};${req.body.liter};${req.body["bubis-e"]}`
+    try{
+        // TODO - delete id line!
+        console.log('id, data', id, data)
+        fs.writeFileSync("udito.txt", data)
+    } catch(e) {
+        res.status(500).json({fileError: e})
+    }
+
+    const responseBody = {id, ...req.body}
+
+    res.status(201).json(responseBody)
+})
+
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'error.html'))
 })
