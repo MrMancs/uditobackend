550ff329db57d7a396023d0e0681ff44ebb61753
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-18 08:03:07 +0200

    Postman L chat

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -4,38 +4,60 @@ const fs = require("fs")
 const app = express()
 const port = 3333
 
-app.use(express.json())
+app.use(express.json)
+
+const uditoDataRead = function(data) {
+    //OK
+    console.log("GET data", data)
+
+    
+    const lines = data.toString().split("\n")
+    console.log("lines", lines)
+
+    const responseBodyArr = lines.map(line=> {
+        
+        if(line.length >= 1) {
+            const elements = line.split(";")
+            return {
+                id: +elements[0],
+                nev: elements[1],
+                liter: +elements[2],
+                "bubis-e": elements[3].trim() == 'true'
+            }
+        } else return undefined  
+    })
+    if (!responseBodyArr[responseBodyArr.length-1]) responseBodyArr.pop()
+
+    console.log("responseBodyArr", responseBodyArr)
+
+    return responseBodyArr
+
+    //TODO
+    res.status(200).json(responseBodyArr)
+}   
 
 app.get("/uditok", (req, res) => {
     fs.readFile("uditok.txt", (err, data) => {
         if (err) res.status(404).json({fileError: err})
         else {
             if (data) {
-                //OK
-                console.log("GET /uditok data", data)
+                const responseBodyArr = uditoDataRead(data)
 
-                
-                const lines = data.toString().split("\n")
-                console.log("lines", lines)
-
-                const responseBodyArr = lines.map(line=> {
-                    
-                    if(line.length >= 1) {
-                        const elements = line.split(";")
-                        return {
-                            id: +elements[0],
-                            nev: elements[1],
-                            liter: +elements[2],
-                            "bubis-e": elements[3].trim() == 'true'
-                        }
-                    } else return undefined  
-                })
-                if (!responseBodyArr[responseBodyArr.length-1]) responseBodyArr.pop()
-
-                console.log("responseBodyArr", responseBodyArr)
-
-                //TODO
                 res.status(200).json(responseBodyArr)
+            } else res.status(404).json({fileError: data})
+        }
+    })
+})
+
+app.get("/udito/:id", (req, res) => {
+    fs.readFile("uditok.txt", (err, data) => {
+        if (err) res.status(404).json({fileError: err})
+        else {
+            if (data) {
+                const responseBodyArr = uditoDataRead(data)
+                const id = +req.params.id
+
+                res.status(200).json(responseBodyArr[id-1])
             } else res.status(404).json({fileError: data})
         }
     })
@@ -45,6 +67,23 @@ app.post('/udito/:id', (req, res) => {
     //{"nev":"Sprite","liter":1,"bubis-e":true}
 
     //TODO - if id exists?
+    fs.readFile("uditok.txt", (err, data) => {
+        if (err) res.status(404).json({fileError: err})
+        else {
+            if (data) {
+                const responseBodyArr = uditoDataRead(data)
+                
+                const id = +req.params.id
+
+                const found = responseBodyArr.find(udito => +udito.id == +id)
+
+                if (responseBodyArr.contains(found)) res.status(300).json({error: "existing id conflict"})
+
+                res.status(300).json({error: "existing id conflict"})
+
+            } //else res.status(404).json({fileError: data}) 
+        }
+    })
 
     //if id doesn't exist yet:
     const id = +req.params.id
